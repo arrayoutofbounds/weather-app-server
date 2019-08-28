8ee61eed67277ef42e7631678283679b0832660e
anmoldesai <anmol.desai@lendi.com.au>
2019-08-28 17:16:57 +1000

    wip

--- src/app.js
+++ src/app.js
@@ -1,24 +1,24 @@
+const path = require("path");
 const express = require("express");
 
-const app = express();
+const app = express(); // create the express application
 
-app.get('', (req, res) => {
-    res.send('<h1>Weather</h1>');
-});
+// customise the server
+// static is a function and then uses what you give it and displays static info. 
+app.use(express.static(path.join(__dirname, '../public'))); 
 
-app.get('/help', (req, res) => {
-    res.send({
-        name: 'anmol',
-        age: 'unknown'
-    })
-})
+// CAN access html pages i.e /about.html
 
-app.get('/about', (req, res) => {
-    res.send("about page")
-})
+// content shown for home page will be index.html.  This is done automatically. route url matches to index.html in the public folder. Hence we do not need the following route
+// app.get('', (req, res) => {
+//     res.send('<h1>Weather</h1>');
+// });
 
 app.get('/weather', (req, res) => {
-    res.send("weather page")
+    res.send([{
+        name: 'anmol',
+        age: 'unknown'
+    }])
 })
 
 app.listen(3000, () => {
