67d18309a1dd9c957cde5633bcda5ef71c11c9b7
Anmol Desai <anmoldesai4@gmail.com>
2019-09-04 21:32:55 +1000

    setup styles

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,11 @@
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+search.value = '';
+messageOne.textContent = 'Loading...';
+messageTwo.textContent = '';
 
 weatherForm.addEventListener('submit', e => {
   e.preventDefault(); // prevent default behaviour of refreshing the page on submit
@@ -9,10 +15,10 @@ weatherForm.addEventListener('submit', e => {
   fetch(`http://localhost:3000/weather?address=${location}`).then(response => {
     response.json().then(data => {
       if (data.error) {
-        console.log(data.error);
+        messageOne.textContent = data.error;
       } else {
-        console.log(data.location);
-        console.log(data.summary);
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.summary;
       }
     });
   });
